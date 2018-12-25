fd7fc732267c8e0665dc07289932a128b7900ded
tkygtr6 <tkygtr6@gmail.com>
2018-12-26 03:18:24 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,13 @@ for file in $(ls -a | grep "^\.[^\.]"); do
     [[ "$file" == ".git" ]] && continue
     [[ "$file" == ".DS_Store" ]] && continue
 
-    ln -si ~/dotfiles/$file ~/$file
+    echo "create ~/$file ? [y]"
+    read ans
+    case $ans in "yes" | "YES" | "y" | "Y" )
+        ln -si ~/dotfiles/$file ~/$file
+        if [ $? = 0 ]; then
+            echo "~/$file created"
+        fi
+        ;;
+    esac
 done

6b69a4f28dea28b32a3ca26c950aebd67765fdd3
tkygtr6 <tkygtr6@gmail.com>
2021-07-23 01:13:38 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,29 @@ for file in $(ls -a | grep "^\.[^\.]"); do
     [[ "$file" == ".git" ]] && continue
     [[ "$file" == ".DS_Store" ]] && continue
 
+    [[ ! -f $file ]] && continue
+
     echo "create ~/$file ? [y]"
     read ans
     case $ans in "yes" | "YES" | "y" | "Y" )
         ln -si ~/dotfiles/$file ~/$file
         if [ $? = 0 ]; then
             echo "~/$file created"
+        fi
+        ;;
+    esac
+done
+
+for file in $(ls -a .config | grep "[^\.]"); do
+    [[ "$file" == ".git" ]] && continue
+    [[ "$file" == ".DS_Store" ]] && continue
+
+    echo "create ~/.config/$file ? [y]"
+    read ans
+    case $ans in "yes" | "YES" | "y" | "Y" )
+        ln -si ~/dotfiles/.config/$file ~/.config/$file
+        if [ $? = 0 ]; then
+            echo "~/.config/$file created"
         fi
         ;;
     esac
